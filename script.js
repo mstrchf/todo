2fe71c9cb6d06a442bb9c2b1a863d66951683275
Pho <phonum5@gmail.com>
2022-01-18 16:12:53 +0000

    todo app

---
+++ script.js
@@ -0,0 +1,105 @@
+// // selecting dom elements
+// let form = document.querySelector('form')
+// let tasks = document.querySelector('.tasks')
+// let input = document.querySelector('input')
+
+// form.addEventListener('submit', (e) => {
+//     // prevent default form behaviour
+//     e.preventDefault();
+
+//     // create new elements
+//     let task = document.createElement('div')
+//     task.classList.add('task')
+
+//     let taskContent = `<div><i class="bx bx-circle"></i><h5>${input.value}</h5></div><button><i id='x' class="bx bx-x"></i></button>`
+
+//     const myFragment = document.createRange().createContextualFragment(taskContent)
+//     task.appendChild(myFragment)
+//     tasks.lastElementChild.before(task)
+//     input.value = '';
+// })
+
+// tasks.addEventListener('click', (e) => {
+//     // remove todo item when x is clicked and mark checkbox and strikethrough text
+//     if (e.target.classList.contains('bx-x')) {
+//         e.target.closest('.task').remove()
+//     } else if (e.target.classList.contains('bx')) {
+//         e.target.classList.toggle('bx-check-circle')
+//         e.target.classList.toggle('bx-circle')
+//         e.target.nextElementSibling.classList.toggle('checked')
+//     }
+
+//     if (e.target.id == 'clear') {
+//         let completed = document.querySelectorAll('.checked')
+
+//         completed.forEach(todo => {
+//             todo.parentElement.parentElement.remove()
+//         });
+//     }
+// })
+
+// document.querySelector('#count').textContent = tasks.children.length - 1
+
+// // tasks.addEventListener('change', e => {
+// //     console.log(tasks.children.length);
+// // })
+
+// let themeToggleBtn = document.querySelector("#toggleBtn");
+// console.log(themeToggleBtn);
+
+// themeToggleBtn.addEventListener("click", (e) => {
+//   alert(themeToggleBtn.getAttributeNames());
+// });
+
+// todos array data
+let todos = [
+    {
+        id: 1,
+        title: "This is my first task",
+        completed: true,
+    },
+    {
+        id: 2,
+        title: "This is my second task",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "This is my third task",
+        completed: false,
+    },
+];
+
+loadTodos(todos)
+
+// selectors
+const todosContainer = document.querySelector(".tasks");
+const tasksLeft = document.getElementById('count');
+const input = document.querySelector('#newTodo')
+
+// event listeners
+
+
+// functions
+
+function loadTodos(array) {
+    let count = 0;
+    array.forEach((todo) => {
+        todosContainer.innerHTML += `<div class="task">
+  <div>
+  ${todo.completed
+                ? '<i class="bx bx-check-circle"></i>'
+                : '<i class="bx bx-circle"></i>'
+            }
+  <h5>${todo.title}</h5>
+  </div>
+  <button><i class="bx bx-x"></i></button>
+</div>`;
+
+        if (!todo.completed) count++
+
+        
+
+    });
+}
+
